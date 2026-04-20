f7059590ff3501dc7fd5048f3c0f0f2a3570e814
Mikhail Rudak <kamaltdinov.ii@mail.ru>
2026-04-20 16:18:08 +0300

    Fix typescript build errors

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -28,7 +28,7 @@ const router = createRouter({
   routes
 })
 
-router.beforeEach((to, from, next) => {
+router.beforeEach((to, _from, next) => {
   const isAuthenticated = !!localStorage.getItem('token')
   if (to.meta.requiresAuth && !isAuthenticated) {
     next('/login')
